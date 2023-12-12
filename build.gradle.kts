adb41d6301102df18b2dc494e4099e88c269ef9b
Vincent Girard <vinccool96@gmail.com>
2023-12-12 13:23:52 -0500

    Switch to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+    application
+}
+
+group = "io.github.vinccool96.observationskt"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    sourceSets.all {
+        languageSettings {
+            languageVersion = "2.0"
+        }
+    }
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
